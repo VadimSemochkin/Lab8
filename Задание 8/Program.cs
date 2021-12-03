8c9b1a41d7ba688c2822640b8f3f5052549ef7e2
Вадим Семочкин <vadimsemochkin96@yandex.ru>
2021-12-03 12:37:11 +0300

    Добавьте файлы проекта.

---
+++ Задание 8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Задание_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "G:/books";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            Console.WriteLine("Список каталогов: ");
+            foreach (var thing in dir.GetDirectories())
+            {
+                Console.WriteLine(thing.Name);
+                Console.WriteLine("Список подкаталогов: ");
+                foreach (var item in thing.GetDirectories())
+               
+                    Console.WriteLine(item.Name);
+                Console.WriteLine();
+               
+            }
+            Console.WriteLine("Список файлов: ");
+            foreach (var item in dir.GetFiles())
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Задача 2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+
+namespace Задача_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Logs/Log.txt";
+            if (!File.Exists(path))
+            {
+                File.Create(path);
+            }
+
+            StreamWriter a = new StreamWriter(path);
+            double[] array = new double[10];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(0,100);
+                
+                a.Write(array[i] + " ");
+            }
+            a.Close();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                Console.Write(sr.ReadLine());
+            }
+            Console.WriteLine();
+            double sum=0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i];
+            }
+            Console.WriteLine("Сумма равна {0}", sum);
+
+            Console.ReadKey();
+        }
+    }
+}
